7867c7b7594c19ebb2b63ed6fea8f7f6418ea920
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-08-31 20:38:39 +0400

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
+        origin:"https://www.rfcollection999.com",
         origin:"https://collection-f.vercel.app",
 
         // origin: "https://www.royasow.store",//مال الفرونت اند
